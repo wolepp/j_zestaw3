ed86ca206af1a4cd443992236aa8572710fa1fd6
Wolepp <wolepp@gmail.com>
2018-10-31 19:38:18 +0100

    added many implementations

--- src/Complex.java
+++ src/Complex.java
@@ -2,6 +2,7 @@ public class Complex implements Field<Complex> {
     private double r, i;
 
     //TODO: użyć @Override w odpowiednich miejscach
+    //TODO: przetestować wszystkie funkcje
 
     public Complex() {
         this(0d);
@@ -21,53 +22,145 @@ public class Complex implements Field<Complex> {
     }
 
 
-//
-//    /* Poniższe metody modyfikują aktualny obiekt i zwracają 'this' */
-//    //TODO: metody niestatyczne mają zwracać this
-//
-//    Complex add(Complex);   // Dodawanie
-//    Complex sub(Complex);   // Odejmowanie
-//    Complex mul(Complex);   // Mnożenie
-//    //TODO: div ma wyrzucać wyjątek przy dzieleni przez 0
-//    Complex div(Complex);   // Dzielenie
-//    double abs();           // Moduł
-//    double sqrAbs();        // Kwadrat modułu
-//    double phase();         // Faza
-//    double re();            // Część rzeczywista
-//    double im();            // Część urojona
-//
-//    /* Metody statyczne dla powyższych operacji */
-//
-//    static Complex add(Complex, Complex);
-//    static Complex sub(Complex, Complex);
-//    static Complex mul(Complex, Complex);
-//    static Complex div(Complex, Complex);
-//    static double abs(Complex);
-//    static double phase(Complex);
-//    static double sqrAbs(Complex);
-//    static double re(Complex);
-//    static double im(Complex);
-//
-//    /* Dodatkowe metody */
-//
+    /* Poniższe metody modyfikują aktualny obiekt i zwracają 'this' */
+
+    public Complex add(Complex c) {     // Dodawanie
+        this.r += c.r;
+        this.i += c.i;
+        return this;
+    }
+    public Complex sub(Complex c) {     // Odejmowanie
+        this.r -= c.r;
+        this.i -= c.i;
+        return this;
+    }
+    public Complex mul(Complex c) {     // Mnożenie
+        double origR = this.r;
+        double origI = this.i;
+        this.r = (origR * c.r) - (origI * c.i);
+        this.i = (origR * c.i) + (origI * c.r);
+        return this;
+    }
+    public Complex div(Complex c) throws ArithmeticException {     // Dzielenie
+        if (c.r == 0 && c.i == 0)
+            throw new ArithmeticException("Dzielenie przez 0");
+        else {
+            double mianownik = (c.r * c.r) + (c.i * c.i);
+            double origR = this.r;
+            double origI = this.i;
+            this.r = (origR * c.r) + (origI * c.i);
+            this.i = (c.r * origI) - (origR * c.i);
+            this.r /= mianownik;
+            this.i /= mianownik;
+        }
+        return this;
+    }
+
+    public double abs() {       // Moduł
+        return Math.sqrt(Math.pow(this.r, 2) + Math.pow(this.i, 2));
+    }
+
+    public double sqrAbs() {   // Kwadrat modułu
+        return (Math.pow(this.r, 2) + Math.pow(this.i, 2));
+    }
+    public double phase() {        // Faza od (-Pi, Pi)
+        //Dokładniejsze wartości jeśli liczba jest postaci z=bi
+        if (this.r == 0) {
+            if (this.i == 0)
+                return 0;
+            else
+                return (this.i > 0) ? Math.PI/2 : -(Math.PI/2);
+        }
+
+        if (this.r > 0)
+            return Math.atan(this.i / this.r);
+        return Math.atan(this.i / this.r) + Math.PI;
+    }
+
+    public static void main(String[] args) {
+        Complex zero = new Complex(0, 0);
+        Complex uno = new Complex(0, 1);
+        Complex c1 = new Complex(2, 3);
+        Complex c2 = new Complex(4, 1);
+        Complex cc = new Complex(add(c1, c2));
+        System.out.println(cc.r + " " + cc.i);
+        System.out.println(c1.r + " " + c1.i);
+        System.out.println(c2.r + " " + c2.i);
+    }
+
+    public double re() {    // Część rzeczywista
+        return this.r;
+    }
+
+    public double im() {    // Część urojona
+        return this.i;
+    }
+
+    /* Metody statyczne dla powyższych operacji */
+
+    public static Complex add(Complex a, Complex b) {
+        return new Complex(a).add(b);
+    }
+
+    public static Complex sub(Complex a, Complex b) {
+        return new Complex(a).sub(b);
+    }
+
+    public static Complex mul(Complex a, Complex b) {
+        return new Complex(a).mul(b);
+    }
+    public static Complex div(Complex a, Complex b) {
+        return new Complex(a).div(b);
+    }
+    public static double abs(Complex c) {
+        return c.abs();
+    }
+
+    public static double phase(Complex c) {
+        return c.phase();
+    }
+    public static double sqrAbs(Complex c) {
+        return c.sqrAbs();
+    }
+    public static double re(Complex c) {
+        return c.re();
+    }
+    static double im(Complex c) {
+        return c.im();
+    }
+
+    /* Dodatkowe metody */
+
+    //TODO: String toString();
 //    String toString();
-//    /* Zwraca String z zapisaną
-//        liczbą zespoloną formacie "-1.23+4.56i" */
-//
+    /* Zwraca String z zapisaną
+        liczbą zespoloną formacie "-1.23+4.56i" */
+
+    //TODO: poprawić ten teścik
     static Complex valueOf(String s) {
         return new Complex(0, 0);
     }
-//    /* Zwraca liczbę zespolona o wartości podanej
-//        w argumencie w formacie "-1.23+4.56i" */
+    /* Zwraca liczbę zespolona o wartości podanej
+        w argumencie w formacie "-1.23+4.56i" */
 //
-//    void setRe(double);
-//    /* Przypisuje podaną wartość części rzeczywistej */
-//
-//    void setIm(double);
-//    /* Przypisuje podaną wartość części urojonej */
-//
-//    void setVal(Complex);
-//    void setVal(double, double);
-//    /* Przypisuje podaną wartość */
-//}
+    void setRe(double r) {
+        this.r = r;
+    }
+    /* Przypisuje podaną wartość części rzeczywistej */
+
+    void setIm(double i) {
+        this.i = i;
+    }
+    /* Przypisuje podaną wartość części urojonej */
+
+    void setVal(Complex c) {
+        this.r = c.r;
+        this.i = c.i;
+    }
+    void setVal(double r, double i) {
+        this.r = r;
+        this.i = i;
+    }
+    /* Przypisuje podaną wartość */
+
 }
